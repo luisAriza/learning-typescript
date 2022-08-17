2ff1df85e05cb629690146c8451c99ec9cf4261a
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-17 15:08:33 -0500

    Tipado inferido

---
+++ src/3-typing.ts
@@ -0,0 +1,12 @@
+(() => {
+  let myProductName = "Product 1"
+  let myProductPrice = 20.00
+
+  myProductName = "Product change"
+  myProductName.toLowerCase()
+
+  myProductPrice.toFixed(1)
+
+  const myProductStock = 1000
+  const myProductName2 = "Product 2"
+})()
