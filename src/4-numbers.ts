eaa7e6ba05f3831f9f44c187757ea00628e55d05
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-17 15:40:51 -0500

    tipo number y boolean

---
+++ src/4-numbers.ts
@@ -0,0 +1,20 @@
+(() => {
+  let productPrice = 100
+  productPrice = 12
+
+  console.log("productPrice", productPrice);
+
+  let customerAge: number = 28
+  // customerAge += "1" //281
+  customerAge += 1
+
+  console.log("customerAge", customerAge);
+
+  let productStock: number
+
+  console.log("productStock", productStock);
+
+  let discount = parseInt("123")
+
+  console.log("discount", discount);
+})()
