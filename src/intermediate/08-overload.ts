1bcb96ab74153bde7ca9e0d194464321b168f9dd
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-20 15:10:39 -0500

    Sobrecarga de funciones: Solución

--- src/intermediate/08-overload.ts
+++ src/intermediate/08-overload.ts
@@ -1,24 +1,30 @@
 // Luis => [L,u,i,s] => string => string[]
 // [L,u,i,s] => Luis => string[] => string
 
-function parseStr(input: string | string[]): string | string[] {
+// type customType = string | string[];
+type customType = unknown;
+
+function parseStr(arg: string): string[]
+function parseStr(arg: string[]): string
+
+function parseStr(input: customType): customType {
   if (Array.isArray(input)) {
     return input.join(''); // string
-  } else {
+  } else if (typeof input === 'string') {
     return input.split(''); // string[]
   }
 }
 
 const rtaArray = parseStr('Luis');
-// rtaArray.reverse();
-if (Array.isArray(rtaArray)) {
-  rtaArray.reverse();
-}
+rtaArray.reverse();
+// if (Array.isArray(rtaArray)) {
+//   rtaArray.reverse();
+// }
 console.log('rtaArray', 'Luis =>', rtaArray);
 
 const rtaStr = parseStr(['L', 'u', 'i', 's']);
-// rtaStr.toLowerCase();
-if (typeof rtaStr === 'string') {
-  rtaStr.toLowerCase();
-}
+rtaStr.toLowerCase();
+// if (typeof rtaStr === 'string') {
+//   rtaStr.toLowerCase();
+// }
 console.log('rtaStr', "['L','u','i','s'] =>", rtaStr);
