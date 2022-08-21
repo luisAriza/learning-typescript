ba5a2005f62c57e3278246a8049e5ff3ec34be11
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-21 11:43:27 -0500

    Interfaces

---
+++ src/intermediate/09-interfaces.ts
@@ -0,0 +1,28 @@
+type Sizes = "S" | "M" | "L" | "XL"
+
+// type Product = {
+//   id: number | string,
+//   title: string,
+//   price: number,
+//   stock: number
+//   size?: Sizes
+// }
+
+interface Product {
+  id: number | string,
+  title: string,
+  price: number,
+  stock: number
+  size?: Sizes
+}
+
+const products: Product[] = []
+
+products.push({
+  id: 1,
+  title: "Cargador",
+  price: 20000,
+  stock: 73,
+})
+
+console.log("products", products);
