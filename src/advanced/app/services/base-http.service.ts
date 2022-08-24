65a9bb025d35ed0e8bb6b95e3a441e771989cb62
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-24 15:42:56 -0500

    Generics en métodos

--- src/advanced/app/services/base-http.service.ts
+++ src/advanced/app/services/base-http.service.ts
@@ -1,35 +1,40 @@
-import axios from "axios"
-import { Category } from "../models/category.model"
-import { Product } from "../models/product.model"
+import axios from 'axios';
+import { UpdateProductDto } from '../dtos/product.dto';
+
+import { Category } from './../models/category.model';
+import { Product } from './../models/product.model';
 
 export class BaseHttpService<TypeClass> {
-  // data: TypeClass[] = []
+  // data: TypeClass[] = [];
+
   constructor(
-    private url: string
+    protected url: string
   ) { }
 
-  async getAll(): Promise<TypeClass[]> {
-    const { data } = await axios.get<TypeClass[]>(this.url)
-    return data
+  async getAll() {
+    const { data } = await axios.get<TypeClass[]>(this.url);
+    return data;
+  }
+
+  async update<ID, DTO>(id: ID, changes: DTO) {
+    const { data } = await axios.put(`${this.url}/${id}`, changes);
+    return data;
   }
 }
 
-// const service = new BaseHttpService<string>()
-// service.getAll()
-
-// const service1 = new BaseHttpService<Category>()
-// service1.getAll()
-
 (async () => {
-  const url = "https://api.escuelajs.co/api/v1/products"
-  const productService = new BaseHttpService<Product>(url)
+  const url1 = 'https://api.escuelajs.co/api/v1/products';
+  const productService = new BaseHttpService<Product>(url1);
 
-  const rta = await productService.getAll()
-  console.log("respuesta", rta);
+  const rta = await productService.getAll();
+  console.log('products', rta.length);
+  productService.update<Product['id'], UpdateProductDto>(1, {
+    title: 'asa',
+  });
 
-  const url2 = "https://api.escuelajs.co/api/v1/categories"
-  const categoryService = new BaseHttpService<Category>(url2)
+  const url2 = 'https://api.escuelajs.co/api/v1/categories';
+  const categoryService = new BaseHttpService<Category>(url2);
 
-  const rta2 = await categoryService.getAll()
-  console.log("respuesta 2", rta2);
-})()
+  const rta1 = await categoryService.getAll();
+  console.log('categories', rta1.length);
+})();
